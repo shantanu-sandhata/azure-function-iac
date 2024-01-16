418821cdb7a52924698ba18131be1c38024235d1
stlind <shantanu.inamdar@sandhata.com>
2024-01-16 17:43:59 +0530

    added provider.tf

---
+++ provider.tf
@@ -0,0 +1,14 @@
+# Azure Provider source and version.
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=3.87.0"
+    }
+  }
+}
+
+# Azure provider configuration block.
+provider "azurerm" {
+  feature {}
+}
