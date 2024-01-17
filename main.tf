06b77476e60804f48beb7728fb7b502c498704b6
stlind <shantanu.inamdar@sandhata.com>
2024-01-17 19:05:52 +0530

    changes to main.tf

--- main.tf
+++ main.tf
@@ -3,8 +3,9 @@ resource "azurerm_resource_group" "sandhata" {
   name     = "sandhata"
   location = "centralindia"
   tags = {
-    "Name" = "Sandhata"
+    "Name"       = "Sandhata"
     "Managed_by" = "Terrafrom"
+    "Project"    = var.project
   }
 }
 
@@ -18,8 +19,9 @@ resource "azurerm_storage_account" "sandhata" {
   account_replication_type = "LRS"
 
   tags = {
-    "Name" = "sandhata"
+    "Name"       = "sandhata"
     "Managed_by" = "Terrafrom"
+    "Project"    = var.project
   }
 }
 
@@ -32,9 +34,51 @@ resource "azurerm_storage_container" "tfstate" {
 
 # Creating azure service plan
 resource "azurerm_service_plan" "asp_sandhata" {
-  name                         = "ASP-sandhata-5e8a"
-  resource_group_name          = resource.azurerm_resource_group.sandhata.name
-  location                     = resource.azurerm_resource_group.sandhata.location
-  os_type                      = "Linux"
-  sku_name                     = "Y1"
+  name                = "ASP-sandhata-5e8a"
+  resource_group_name = resource.azurerm_resource_group.sandhata.name
+  location            = resource.azurerm_resource_group.sandhata.location
+  os_type             = "Linux"
+  sku_name            = "Y1"
+
+  tags = {
+    "Name"       = "ASP-sandhata-5e8a"
+    "Managed_by" = "Terraform"
+    "Project"    = var.project
+  }
+
+}
+
+# Creating a log analytics workspace
+resource "azurerm_log_analytics_workspace" "sandhata" {
+  name                = "DefaultWorkspace-6f6b6f42-7704-42fa-b07a-9db961090f1b-CIN"
+  resource_group_name = "defaultresourcegroup-cin"
+  location            = "centralindia"
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+
+  tags = {
+    "Name" = "DefaultWorkspace-6f6b6f42-7704-42fa-b07a-9db961090f1b-CIN"
+    "Managed_by" = "Terraform"
+    "Project" = var.project
+  }
+}
+
+# Creating a app insight resource
+resource "azurerm_application_insights" "sandhata" {
+  name                = "sandhata\\"
+  resource_group_name = resource.azurerm_resource_group.sandhata.name
+  location            = resource.azurerm_resource_group.sandhata.location
+  application_type    = "web"
+  sampling_percentage = 0
+  workspace_id        = "/subscriptions/6f6b6f42-7704-42fa-b07a-9db961090f1b/resourceGroups/DefaultResourceGroup-CIN/providers/Microsoft.OperationalInsights/workspaces/DefaultWorkspace-6f6b6f42-7704-42fa-b07a-9db961090f1b-CIN"
+
+  tags = {
+    "Name" = "${var.default_name}\\"
+    "Managed_by" = "Terraform"
+    "Project" = var.project
+  }
+}
+
+# Creating action group to send notifications
+resource "azurerm_monitor_action_group" "funcappag" {
 }
