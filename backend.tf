dd49d0a00e8112c7e6ee36c7c80b45aae5b24448
stlind <shantanu.inamdar@sandhata.com>
2024-01-17 18:58:59 +0530

    changes to backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
-    backend "azurerm" {
-      resource_group_name = "sandhata"
-      storage_account_name = "sandhata"
-      container_name = "tfstate-sandhatafunc"
-      key = "sandhatafunc.tfstate"
-    }
+  backend "azurerm" {
+    resource_group_name  = "sandhata"
+    storage_account_name = "sandhata"
+    container_name       = "tfstate-sandhatafunc"
+    key                  = "sandhatafunc.tfstate"
+  }
 }
