782c731da1de4132b6c5c5ba164bf3475885e65f
stlind <shantanu.inamdar@sandhata.com>
2024-01-17 18:57:19 +0530

    adding variables.tf

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "location" {
+  type    = string
+  default = "centralindia"
+}
+
+variable "default_name" {
+  type    = string
+  default = "sandhata"
+}
+
+variable "project" {
+  type    = string
+  default = "azure_poc"
+}
